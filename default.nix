290dfd2b126a6c5fc5d497a9f6fff2099e839357
Neil Justice <neil.justice@cse.org.uk>
2020-11-24 15:30:22 +0000

    revert nix changes; minor changes to cli

--- default.nix
+++ default.nix
@@ -1,5 +1,28 @@
-with import <nixpkgs> {};
-
 let
-    solar_pv = callPackage ./build.nix {};
-in python37.withPackages (ps: [ solar_pv ] )
+  pkgs = import <nixpkgs> {};
+  saga_albion = (import ./320-albion-saga-gis/default.nix) { inherit pkgs; };
+in with pkgs;
+
+stdenv.mkDerivation rec {
+  name = "albion-solar-pv";
+  version = "0.1";
+
+  buildInputs = [
+    (python37.withPackages (pps: with pps; [
+      psycopg2
+      requests
+      gdal
+      numpy
+    ]))
+    saga_albion
+  ];
+
+  env = buildEnv {
+    name = name;
+    paths = buildInputs;
+  };
+
+  builder = builtins.toFile "builder.sh" ''
+    source $stdenv/setup; ln -s $env $out
+  '';
+}
