18252def5ba647ec43a1675f64cd6c874fc06915
Neil Justice <neil.justice@cse.org.uk>
2021-09-30 15:07:21 +0100

    added comments and cleaned up method names in pv_gis_client.py

--- albion_models/solar_pv/pv_gis/pv_gis_client.py
+++ albion_models/solar_pv/pv_gis/pv_gis_client.py
@@ -25,16 +25,10 @@ _ALLOWED_ERRORS = ("Location over the sea. Please, select another location",)
 _session: requests.Session
 
 
-def init_process():
-    global _session
-    _session = requests.Session()
-    # Retry on things like socket, timeout, connection errors, not HTTP error codes:
-    adapter = HTTPAdapter(max_retries=5)
-    _session.mount('http://', adapter)
-    _session.mount('https://', adapter)
-
-
 def pv_gis(pg_uri: str, job_id: int, peak_power_per_m2: float, pv_tech: str, solar_dir: str):
+    """
+    Module entrypoint: run each roof plane through PVGIS.
+    """
     solar_pv_csv = join(solar_dir, 'solar_pv.csv')
     pg_conn = connect(pg_uri, cursor_factory=psycopg2.extras.DictCursor)
     try:
@@ -42,10 +36,10 @@ def pv_gis(pg_uri: str, job_id: int, peak_power_per_m2: float, pv_tech: str, sol
             cursor.execute(SQL("SELECT * FROM {panel_horizons} WHERE usable = true").format(
                 panel_horizons=Identifier(tables.schema(job_id), tables.PANEL_HORIZON_TABLE))
             )
-            rows = cursor.fetchall()
+            roof_planes = cursor.fetchall()
             pg_conn.commit()
-            logging.info(f"{len(rows)} queries to send:")
-            _solar_pv_estimate(rows, peak_power_per_m2, pv_tech, solar_pv_csv)
+            logging.info(f"{len(roof_planes)} queries to send:")
+            _estimate_solar_pv(roof_planes, peak_power_per_m2, pv_tech, solar_pv_csv)
     finally:
         pg_conn.close()
 
@@ -67,19 +61,40 @@ def _write_results_to_db(pg_uri: str, job_id: int, csv_file: str):
         pg_conn.close()
 
 
-def _solar_pv_estimate(iterable: Iterable[dict],
+def init_process():
+    """
+    Function passed to the multiprocessing pool as the `initializer` arg, that will
+    be run on startup by each subprocess.
+    """
+    # Each process will have it's own version of this global value:
+    global _session
+    _session = requests.Session()
+    # Retry on things like socket, timeout, connection errors, not HTTP error codes:
+    adapter = HTTPAdapter(max_retries=5)
+    _session.mount('http://', adapter)
+    _session.mount('https://', adapter)
+
+
+def _estimate_solar_pv(roof_planes: Iterable[dict],
                        peak_power_per_m2: float,
                        pv_tech: str,
                        out_filename: str,
                        log_frequency: int = 250):
-    with open(out_filename, 'w') as out, mp.Pool(_WORKERS, initializer=init_process) as pool:
+    """
+    Estimate the solar PV output for an Iterable of rows from the `panel_horizons`
+    database table.
+
+    This is performed as a parallel operation and results are written to a CSV file.
+    """
+    with open(out_filename, 'w') as out, \
+            mp.Pool(_WORKERS, initializer=init_process) as pool:
         csv_writer = None
         processed: int = 0
         errors: int = 0
 
         wrapped_iterable = (dict(row,
                                  peak_power_per_m2=peak_power_per_m2,
-                                 pv_tech=pv_tech) for row in iterable)
+                                 pv_tech=pv_tech) for row in roof_planes)
         for res in pool.imap_unordered(_handle_row, wrapped_iterable, chunksize=10):
             if res is not None:
                 if csv_writer is None:
@@ -95,59 +110,56 @@ def _solar_pv_estimate(iterable: Iterable[dict],
         print(f"Total allowed PV-GIS errors: {errors}")
 
 
-# PV-GIS API params, from https://ec.europa.eu/jrc/en/PVGIS/docs/noninteractive
-# More detail on them is in the user manual https://ec.europa.eu/jrc/en/PVGIS/docs/usermanual
-# Name 	                Type* 	Obligatory 	Default 	    Comments
-# lat           	    F 	    Yes 	    - 	            Latitude, in decimal degrees, south is negative.
-# lon 	                F 	    Yes 	    -       	    Longitude, in decimal degrees, west is negative.
-# usehorizon 	        I 	    No 	        1 	            Calculate taking into account shadows from high horizon. Value of 1 for "yes".
-# userhorizon 	        L 	    No 	        - 	            Height of the horizon at equidistant directions around the point of interest, in degrees.
-#                                                           Starting at north and moving clockwise. The series '0,10,20,30,40,15,25,5' would mean the
-#                                                           horizon height is 0° due north, 10° for north-east, 20° for east, 30° for south-east, etc.
-# raddatabase 	        T 	    No          'PVGIS-SARAH'   Name of the radiation database. "PVGIS-SARAH" for Europe, Africa and Asia or "PVGIS-NSRDB"
-#                                                           for the Americas between 60°N and 20°S,  "PVGIS-ERA5" and "PVGIS-COSMO" for Europe (including
-#                                                           high-latitudes), and "PVGIS-CMSAF" for Europe and Africa (will be deprecated)
-# peakpower 	        F 	    Yes 	    - 	            Nominal power of the PV system, in kW.
-# pvtechchoice 	        T 	    No      	"crystSi" 	    PV technology. Choices are: "crystSi", "CIS", "CdTe" and "Unknown".
-# mountingplace 	    T 	    No      	"free"      	Type of mounting of the PV modules. Choices are: "free" for free-standing and "building" for building-integrated.
-# loss 	                F 	    Yes     	- 	            Sum of system losses, in percent.
-# fixed          	    I 	    No      	1 	            Calculate a fixed mounted system. Value of 0 for "no". All other values (or no value)
-#                                                           mean "Yes". Note that this means the default is "yes".
-# angle         	    F 	    No 	        0 	            Inclination angle from horizontal plane of the (fixed) PV system.
-# aspect 	            F 	    No 	        0 	            Orientation (azimuth) angle of the (fixed) PV system, 0=south, 90=west, -90=east.
-# optimalinclination 	I 	    No 	        0 	            Calculate the optimum inclination angle. Value of 1 for "yes". All other values (or no value) mean "no".
-# optimalangles 	    I 	    No 	        0 	            Calculate the optimum inclination AND orientation angles. Value of 1 for "yes". All other
-#                                                           values (or no value) mean "no".
-# inclined_axis 	    I 	    No 	        0 	            Calculate a single inclined axis system. Value of 1 for "yes". All other values (or no value) mean "no".
-# inclined_optimum 	    I 	    No 	        0 	            Calculate optimum angle for a single inclined axis system. Value of 1 for "yes". All other values
-#                                                           (or no value) mean "no".
-# inclinedaxisangle 	F 	    No 	        0 	            Inclination angle for a single inclined axis system. Ignored if the optimum angle should be
-#                                                           calculated (parameter "inclined_optimum").
-# vertical_axis 	    I 	    No 	        0 	            Calculate a single vertical axis system. Value of 1 for "yes". All other values (or no value) mean "no".
-# vertical_optimum 	    I 	    No 	        0 	            Calculate optimum angle for a single vertical axis system. Value of 1 for "yes". All other values
-#                                                           (or no value) mean "no".
-# verticalaxisangle 	F 	    No 	        0 	            Inclination angle for a single vertical axis system. Ignored if the optimum angle should be
-#                                                           calculated (parameter "vertical_optimum" set to 1).
-# twoaxis 	            I 	    No 	        0 	            Calculate a two axis tracking system. Value of 1 for "yes". All other values (or no value) mean "no".
-# pvprice 	            I 	    No 	        0 	            Calculate the PV electricity price [kwh/year] in the currency introduced by the user for the system cost.
-# systemcost 	        F 	    if pvprice 	- 	            Total cost of installing the PV system [your currency].
-# interest 	            F 	    if pvprice 	- 	            Interest in %/year
-# lifetime 	            I 	    No 	        25 	            Expected lifetime of the PV system in years.
-# outputformat 	        T 	    No 	        "csv" 	        Type of output. Choices are: "csv" for the normal csv output with text explanations,
-#                                                           "basic" to get only the data output with no text, and "json".
-# browser 	            I 	    No 	        0 	            Use this with a value of "1" if you access the web service from a web browser and want to save the data to a file.
-#
-# * F = Floating point number; I = integer number; L = list of numbers; T= text string.
+def _handle_row(row: Dict[str, str]) -> Optional[dict]:
+    """
+    For a given row from the `panel_horizons` table, query PVGIS and shape
+    the outputs into a useful object, unless there's been an error, in which
+    case return None.
+    """
+    try:
+        lon, lat, horizon, angle, aspect, peakpower, loss, pv_tech = _row_to_pv_gis_params(row)
+
+        start_time = time.time()
+        response = _get_pvgis(lon, lat, horizon, angle, aspect, peakpower, loss, pv_tech)
+        if response is None:
+            return None
+
+        time_taken = time.time() - start_time
+        # Stay under the API rate limit:
+        if time_taken < _API_RATE_LIMIT_SECONDS_PER_WORKER:
+            time.sleep(_API_RATE_LIMIT_SECONDS_PER_WORKER - time_taken)
+
+        results = flatten(response['outputs'])
+        results.update({
+            'easting': row['easting'],
+            'northing': row['northing'],
+            'toid': row['toid'],
+            'roof_plane_id': row['roof_plane_id'],
+            'peak_power': response['inputs']['pv_module']['peak_power'],
+            'horizon_sd': row['horizon_sd'],
+            'southerly_horizon_sd': row['southerly_horizon_sd'],
+        })
+        return results
+    except Exception as e:
+        print('Caught exception in worker process:')
+        traceback.print_exc()
+        print()
+        raise e
 
 
-def _single_solar_pv_estimate(lon: float,
-                              lat: float,
-                              horizon: List[float],
-                              angle: float,
-                              aspect: float,
-                              peakpower: float,
-                              loss: float,
-                              pvtechchoice: str) -> Optional[dict]:
+def _get_pvgis(lon: float,
+               lat: float,
+               horizon: List[float],
+               angle: float,
+               aspect: float,
+               peakpower: float,
+               loss: float,
+               pvtechchoice: str) -> Optional[dict]:
+    """
+    Make a single request to PVGIS.
+    * PV-GIS API params are here: https://ec.europa.eu/jrc/en/PVGIS/docs/noninteractive
+    * More detail on them in the user manual: https://ec.europa.eu/jrc/en/PVGIS/docs/usermanual
+    """
     global _session
     url = 'https://re.jrc.ec.europa.eu/api/PVcalc'
     res = _session.get(url, params={
@@ -179,39 +191,10 @@ def _single_solar_pv_estimate(lon: float,
     return body
 
 
-def _handle_row(row: Dict[str, str]) -> Optional[dict]:
-    try:
-        lon, lat, horizon, angle, aspect, peakpower, loss, pv_tech = _row_to_pv_gis_params(row)
-
-        start_time = time.time()
-        response = _single_solar_pv_estimate(lon, lat, horizon, angle, aspect, peakpower, loss, pv_tech)
-        if response is None:
-            return None
-
-        time_taken = time.time() - start_time
-        # Stay under the API rate limit:
-        if time_taken < _API_RATE_LIMIT_SECONDS_PER_WORKER:
-            time.sleep(_API_RATE_LIMIT_SECONDS_PER_WORKER - time_taken)
-
-        results = flatten(response['outputs'])
-        results.update({
-            'easting': row['easting'],
-            'northing': row['northing'],
-            'toid': row['toid'],
-            'roof_plane_id': row['roof_plane_id'],
-            'peak_power': response['inputs']['pv_module']['peak_power'],
-            'horizon_sd': row['horizon_sd'],
-            'southerly_horizon_sd': row['southerly_horizon_sd'],
-        })
-        return results
-    except Exception as e:
-        print('Caught exception in worker process:')
-        traceback.print_exc()
-        print()
-        raise e
-
-
 def _row_to_pv_gis_params(row: dict) -> tuple:
+    """
+    Convert a `panel_horizons` row into PVGIS API params.
+    """
     lon, lat = _easting_northing_to_lon_lat(row['easting'], row['northing'])
 
     # SAGA and PV-GIS both expect starting at North, moving clockwise
