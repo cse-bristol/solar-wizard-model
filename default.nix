0c9cc41cd12a14f269320f4aa9e741a03e905ee7
Neil Justice <neil.justice@cse.org.uk>
2023-09-18 16:16:23 +0100

    update default.nix

--- default.nix
+++ default.nix
@@ -3,6 +3,18 @@
 let
   pkgs = (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/22.05.tar.gz") {});
   grass_pvmaps = pkgs.callPackage ./nix/grass-8.2.0-pvmaps.nix {};
+  shapely = pkgs.python310.pkgs.buildPythonPackage rec {
+    pname = "shapely";
+    version = "2.0.1";
+    src = pkgs.python310.pkgs.fetchPypi {
+      inherit pname version;
+      hash = "sha256:14v88k0y7qhp8n5clip6w96pkdzrfqa2hsjkhpy9gkifwyiv39k6";
+    };
+    nativeBuildInputs = [ pkgs.python310.pkgs.cython pkgs.geos pkgs.python310.pkgs.setuptools ];
+    buildInputs = [ pkgs.geos ];
+    propagatedBuildInputs = [ pkgs.python310.pkgs.numpy ];
+    doCheck = false;
+  };
 in
 pkgs.stdenv.mkDerivation rec {
   name = "solar-wizard-model";
@@ -16,7 +28,7 @@ pkgs.stdenv.mkDerivation rec {
       pps.numpy
       pps.scikitlearn
       pps.scikitimage
-      pps.shapely
+      shapely
     ]))
     pkgs.postgis
     pkgs.py-spy  # for profiling
